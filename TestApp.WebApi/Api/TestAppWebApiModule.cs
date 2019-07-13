cd0af444f971dc71e3bb78a0a87152e4bffc212a
josegcb <josegcb@gmail.com>
2019-07-13 11:24:10 -0400

    Add files via upload

---
+++ TestApp.WebApi/Api/TestAppWebApiModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Http;
+using Abp.Application.Services;
+using Abp.Configuration.Startup;
+using Abp.Domain.Repositories;
+using Abp.Modules;
+using Abp.WebApi;
+using Castle.Core;
+using Castle.MicroKernel.Registration;
+using Library.Repositories;
+using Library.Services;
+using TestApp.EntityFramework.Repositories;
+using TestApp.Managers;
+using TestApp.Managers.Interface;
+using TestApp.Models;
+using TestApp.Services;
+
+namespace TestApp.Api
+{
+    [DependsOn(typeof(AbpWebApiModule), typeof(TestAppApplicationModule))]
+    public class TestAppWebApiModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+            IocManager.IocContainer.Register(Component.For(typeof(IManagerBase<>)).ImplementedBy(typeof(ManagerBase<>)).LifestylePerWebRequest());
+
+            IocManager.IocContainer.Register(Component.For(typeof(IManagerBase<Comprobante>)).ImplementedBy(typeof(ComprobanteManager<>)).LifestylePerWebRequest());
+            //Registro de manager con Interface propia
+            IocManager.IocContainer.Register(Component.For<IManagerBase<Cuenta>, ICuentaManager>().ImplementedBy(typeof(CuentaManager<>)).LifestylePerWebRequest());
+
+
+
+
+            Configuration.Modules.AbpWebApi().DynamicApiControllerBuilder
+                .ForAll<IApplicationService>(typeof(TestAppApplicationModule).Assembly, "app")
+                .Build();
+
+            Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+            Library.Helpers.SwaggerHelper.ConfigureSwaggerUi<TestAppWebApiModule>(Configuration, "TestApp.WebApi");
+            Configuration.Modules.AbpWebCommon().SendAllExceptionsToClients = false;
+
+        }
+
+        public override void PreInitialize() {
+            base.PreInitialize();
+            IocManager.IocContainer.Register(Component.For(typeof(ITestAppGenericRepository<>)).ImplementedBy(typeof(TestAppRepositoryBase<>)).LifestylePerWebRequest());                    }
+    }
+
+}

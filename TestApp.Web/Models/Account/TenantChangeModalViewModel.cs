971e2bb5e63fbd6b03e4fb309c0d0f5a85844436
josegcb <josegcb@gmail.com>
2019-07-13 11:51:04 -0400

    Add files via upload

---
+++ TestApp.Web/Models/Account/TenantChangeModalViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TestApp.Web.Models.Account
+{
+    public class TenantChangeModalViewModel
+    {
+        public string TenancyName { get; set; }
+    }
+}
---
+++ TestApp.Web/Models/Account/TenantChangeViewModel.cs
@@ -0,0 +1,11 @@
+using Abp.AutoMapper;
+using TestApp.Sessions.Dto;
+
+namespace TestApp.Web.Models.Account
+{
+    [AutoMapFrom(typeof(GetCurrentLoginInformationsOutput))]
+    public class TenantChangeViewModel
+    {
+        public TenantLoginInfoDto Tenant { get; set; }
+    }
+}
